6dc5c7cd96d48dc7e90c6367a737be5f57426159
WWWWM03 <74545085+WWWWM03@users.noreply.github.com>
2022-11-20 20:00:56 +0800

    Add files via upload

---
+++ tgbot-ptb/event.py
@@ -0,0 +1,18 @@
+import logging
+from typing import Dict, Any
+import threading
+from mbot.core.event.models import EventType
+from mbot.core.plugins import plugin
+from mbot.core.plugins import PluginContext, PluginMeta
+from .ptbsingle import TgBotSub
+
+_LOGGER = logging.getLogger(__name__)
+tgbot = TgBotSub()
+
+
+@plugin.after_setup
+def main(plugin: PluginMeta, config: Dict):
+    tgbot.set_config(config.get('TGbotTOKEN'), config.get('chat_id') , config.get('proxy'))
+    _LOGGER.info(f"{plugin.manifest.title}加载成功，chatid_list：{config.get('TGbotTOKEN')},TGbotTOKEN：{config.get('chat_id')},Proxy：{config.get('proxy')}")
+    thread = threading.Thread(target=tgbot.start_bot, args=(config,))
+    thread.start()
