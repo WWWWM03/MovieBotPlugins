de9ed93817cdd93e5bd12054fae77a699422e8a6
WWWW <635069200@qq.com>
2022-12-06 00:12:36 +0800

    fix bugs

--- discord/example_bot.py
+++ discord/example_bot.py
@@ -10,8 +10,9 @@ plex = PlexServer(baseurl, token)
 app = Presence("1049283090255712276")
 app.connect()
 
-
+starttime = time.time()
 def _update():
+
     sessions = plex.sessions()
     if sessions == []:
         app.update(state="摸鱼ing....", large_image="plex")
@@ -27,15 +28,14 @@ def _update():
     # second = viewOffset % 60
     # minute = (viewOffset - viewOffset // 3600 * 3600) // 60
     # hour = viewOffset // 3600
-    # remaining_duration = round(float(sessions.duration) - viewOffset / 60, 0)
-    # endtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sessions.viewOffset))
-    # endtime = time.mktime(time.strptime(str(endtime), '%Y-%m-%d %H:%M:%S'))
+    # remaining_duration = (float(sessions.duration) - viewOffset) / 60
+    # endtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sessions.viewOffset + time.time()))
+    # endtime = sessions.viewOffset + time.time()
     app.update(state=title, details="正在播放",
                large_image=photo, large_text="MovieBot",
-               small_image="moviebot", small_text="MovieBot"
-               )
-
+               small_image="moviebot", small_text="MovieBot")
 def loop_func(func, second):
+    print('开始轮询')
     while True:
         timer = Timer(second, func)
         timer.start()
